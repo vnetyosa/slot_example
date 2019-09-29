2dddf7750ce650bb51f6c2cb186a99c9f363cb66
Vladimir Netyosa <v.netyosa@Vladimirs-MacBook-Pro.local>
2019-09-29 18:52:29 +0300

    init

--- src/code/index.ts
+++ src/code/index.ts
@@ -1,3 +1,28 @@
-window.addEventListener("DOMContentLoaded", (): void => {
-    alert("How are you doing, mate?");
-});
+import { MainGameView } from "./main/MainGameView";
+import { MainGameController } from "./main/MainGameController";
+import { MainGameModel } from "./main/MainGameModel";
+
+const renderer = PIXI.autoDetectRenderer(1200, 800);
+document.body.appendChild(renderer.view);
+
+//window.addEventListener("DOMContentLoaded", (): void => {
+//    alert("How are you doing, mate?");
+// });
+
+const view  = new MainGameView();
+const controller = new MainGameController();
+const model = new MainGameModel();
+
+view.model = model;
+view.controller = controller;
+
+controller.model = model;
+controller.view = view;
+
+function animate() {
+    // render the stage
+    renderer.render(view.container);
+    view.animate();
+    requestAnimationFrame(animate);
+}
+animate();
